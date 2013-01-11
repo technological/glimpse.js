4119564211be688318444fb765b0202348f2595a
Ed Rooth <ed.rooth@gmail.com>
2013-01-11 15:26:35 -0800

    recovered misplaced file contents

--- src/core/object.js
+++ src/core/object.js
@@ -1,26 +1,37 @@
-define(
-function () {
+define([
+  'core/array'
+],
+function (array) {
   'use strict';
 
-  var Obj = {
-    propertyDefaults: {
-      writable: true,
-      enumerable: true,
-      configurable: true
-    },
-    create: function (parent) {
-      var newObj = Object.create(parent || null);
-      if (typeof newObj.init === 'function') {
-        newObj.init();
+  var Obj;
+
+  Obj = {
+
+    // get a completely empty object with no prototype
+    empty: function (props) {
+      var obj = Object.create(null);
+      if (props) {
+        Obj.extend(obj, props);
       }
-      return newObj;
+      return obj;
     },
-    defineProperty: function (obj, name, value) {
-      var config = this.propertyDefaults;
-      config.value = value;
-      Object.defineProperty(obj, name, config);
+
+    // similar to _.extend() but es5 safe
+    extend: function (target) {
+      var sources = array.convertArgs(arguments, 1);
+      sources.forEach(function (src) {
+        if (typeof src !== 'object') {
+          return;
+        }
+        Object.getOwnPropertyNames(src).forEach(function (propName) {
+          Object.defineProperty(target, propName,
+            Object.getOwnPropertyDescriptor(src, propName));
+        });
+      });
+      return target;
     }
-    //mixin: function () {}
+
   };
 
   return Obj;
