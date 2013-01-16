79cecc5d1ec002f725f102aed2317a73ed20912d
Ed Rooth <ed.rooth@gmail.com>
2013-01-15 16:21:53 -0800

    feat($components.legend) Unit tests and refactoring of legend component.
    Other minor changes:
    - Modified the example to be more like the mocks.
    - Added 2nd smaller examlp graph to showcase scaling and make sure 2 graphs can
      be rendered simultaneously.
    - Updated line to pass color from its own config.
    - Fixed the code in graphs.graph that adds to legend.
    - Fixed bug in graph.component(). Added option to retrieve all components too.
    - Fixed bug in string spec.

--- src/components/legend.js
+++ src/components/legend.js
@@ -1,79 +1,212 @@
+/**
+ * @fileOverview
+ * A Legend component that displays keys containing color indicators and the
+ * label text of data.
+ */
 define([
   'core/object',
-  'core/config'
+  'core/config',
+  'core/string',
+  'd3-ext/util'
 ],
-function (obj, config) {
+function(obj, config, string, util) {
   'use strict';
 
-  return function () {
+  return function() {
 
-    var config_ = {},
-      defaults_ = {
-        isFramed: false,
-        radius: 4,
-        textColor: '#333',
-        spacing: 4,
-        marginLeft: 4,
-        marginTop: 16
-      },
+    // PRIVATE
+
+    var defaults_,
+      config_,
       data_,
-      root_;
+      root_,
+      enter_,
+      update_,
+      remove_,
+      reposition_;
 
+    config_ = {};
+
+    defaults_ = {
+      id: string.random(),
+      isFramed: false,
+      indicatorWidth: 10,
+      indicatorHeight: 10,
+      indicatorSpacing: 4,
+      fontColor: '#333',
+      fontFamily: 'Arial, sans-serif',
+      fontWeight: 'bold',
+      fontSize: 13,
+      keySpacing: 20,
+      marginTop: 10,
+      marginRight: 0,
+      marginBottom: 0,
+      marginLeft: 10,
+      keys: []
+    };
+
+    /**
+     * Inserts new keys.
+     * @param {d3.selection} selection
+     */
+    enter_ = function(selection) {
+      var enterSelection;
+
+      enterSelection = selection
+        .enter()
+          .append('g')
+          .attr({
+            'class': 'gl-legend-key'
+          });
+
+      // Add new key indicator.
+      enterSelection
+        .append('rect')
+        .attr({
+          'class': 'gl-legend-key-indicator',
+          'stroke': 'none',
+          'x': 0,
+          'y': 0
+        });
+
+      // Add new key text.
+      enterSelection
+        .append('text')
+        .attr({
+          'class': 'gl-legend-key-label',
+          'text-anchor': 'start',
+          'stroke': 'none'
+        });
+    };
+
+    /**
+     * Apply updates to the update selection.
+     * @param {d3.selection} selection
+     */
+    update_ = function(selection) {
+      // The outer <g> element for each key.
+      selection
+        .attr({
+          'class': 'gl-legend-key',
+          'font-family': config_.fontFamily,
+          'font-size': config_.fontSize,
+          'font-weight': config_.fontWeight
+        });
+
+      // Update key indicators.
+      selection.selectAll('.gl-legend-key-indicator')
+        .attr({
+          'width': config_.indicatorWidth,
+          'height': config_.indicatorHeight,
+          'fill': function(d) { return d.color; }
+        });
+
+      // Update key text.
+      selection.selectAll('.gl-legend-key-label')
+        .text(function(d) { return d.label; })
+        .attr({
+          'x': config_.indicatorWidth + config_.indicatorSpacing,
+          'y': config_.indicatorHeight,
+          'fill': config_.fontColor
+        });
+    };
+
+    /**
+     * Iterate thru each key and apply proper translation based on
+     * previous text width.
+     * @param {d3.selection} selection
+     */
+    reposition_ = function(selection) {
+      var keyYTranslate = config_.marginLeft;
+
+      selection.each(function() {
+        d3.select(this).attr('transform',
+          function() {
+            var prevNode = this.previousElementSibling;
+            if (prevNode) {
+              keyYTranslate += config_.indicatorWidth +
+                config_.keySpacing +
+                config_.indicatorSpacing +
+                prevNode.childNodes[1].getBBox().width;
+            }
+            return 'translate(' + [keyYTranslate, config_.marginTop] + ')';
+          });
+      });
+    };
+
+    /**
+     * Remove any keys that were removed.
+     * @param {d3.selection} selection
+     */
+    remove_ = function(selection) {
+      selection.exit().remove();
+    };
+
+    // PUBLIC
+
+    /**
+     * The main function.
+     */
     function legend() {
       obj.extend(config_, defaults_);
-
       return legend;
     }
 
-    legend.update = function () {
-      var previousX;
-      previousX = config_.marginLeft;
+    /**
+     * Apply post-render updates.
+     * Insert/update/remove DOM for each key.
+     */
+    legend.update = function() {
+      var selection;
 
-
-      // TODO: remove any existing stuff 1st
-
+      // Return early if no data or render() hasn't been called yet.
       if (!config_.keys || !root_) {
         return;
       }
-      config_.keys.forEach(function (key) {
-        var group, text, circle;
-        group = root_.append('g')
-          .attr({
-            'transform': 'translate(' + previousX + ')'
-          });
-        text = group.append('text')
-          .text(key.title)
-          .attr({
-            'x': config_.radius * 4,
-            'y': config_.marginTop,
-            'stroke': 'none',
-            'fill': config_.textColor,
-            'text-anchor': 'start'
-          });
-        circle = group.append('circle')
-          .attr({
-            'stroke': 'none',
-            'fill': key.color,
-            'r': config_.radius,
-            'cx': config_.radius * 2,
-            'cy': Math.max(config_.marginTop - config_.radius, 0)
-          });
-        previousX += text[0][0].scrollWidth + config_.spacing;
+
+      root_.attr({
+        'class': 'gl-component gl-legend',
+        'id': config_.id
       });
 
+      // The selection of legend keys.
+      selection = root_
+        .selectAll('.gl-legend-key')
+        .data(config_.keys, function(d) { return d.color; });
+
+      remove_(selection);
+      enter_(selection);
+      update_(selection);
+      reposition_(selection);
       return legend;
     };
 
-    legend.render = function (selection) {
-      root_ = selection.append('g')
-        .attr({
-          'class': 'component legend id-' + config_.id
-        });
+    /**
+     * Render the legend to the selection.
+     * Sets up initial DOM structure. Should only be called once.
+     * @param {d3.selection|String} selection A d3 selection
+     *    or a selector string.
+     */
+    legend.render = function(selection) {
+      root_ = util.select(selection).append('g');
       legend.update();
       return legend;
     };
 
-    obj.extend(legend, config(legend, config_, []));
+    // MIXINS
+    obj.extend(
+      legend,
+      config(legend, config_, [
+        'id',
+        'keys',
+        'fontColor',
+        'fontFamily',
+        'fontSize',
+        'fontWeight',
+        'indicatorWidth',
+        'indicatorHeight'
+      ]));
+
     return legend();
   };
 
