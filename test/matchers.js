b05a9740e121cb7a437867d4a2485df336345780
Ed Rooth <ed.rooth@rackspace.com>
2013-01-17 17:20:21 -0800

    chore($testrunner) Added matcher for class names.

--- test/matchers.js
+++ test/matchers.js
@@ -13,12 +13,25 @@ beforeEach(function() {
       return typeof this.actual === type;
     },
 
-    toHaveProperties: function (name0, name1, name2) {
+    toHaveProperties: function() {
       var actual = this.actual,
         len = arguments.length,
         i;
       for (i = 0; i < len; i += 1) {
         if (actual[arguments[i]] === undefined) {
+          return false;
+        }
+      }
+      return true;
+    },
+
+    toHaveClasses: function() {
+      var actual = this.actual,
+        len = arguments.length,
+        classes = Array.prototype.slice.call(actual.classList),
+        i;
+      for (i = 0; i < len; i += 1) {
+        if (classes.indexOf(arguments[i]) === -1) {
           return false;
         }
       }
