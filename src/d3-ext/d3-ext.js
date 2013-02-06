6dd6b845c1a0642be6ad221b4b454de61786adfc
Johann Verghese <johann.verghese@gmail.com>
2013-02-06 13:18:54 -0800

    Add d3 helper functions for: layout, position, size.

---
+++ src/d3-ext/d3-ext.js
@@ -0,0 +1,9 @@
+define([
+  'd3',
+  'd3-ext/size',
+  'd3-ext/layout',
+  'd3-ext/position'
+], function(d3) {
+  'use strict';
+  return d3;
+});
---
+++ test/unit/d3-ext/layout.spec.js
@@ -0,0 +1,99 @@
+define([
+  'd3-ext/d3-ext'
+],
+function(d3) {
+  'use strict';
+
+  describe('selection.layout', function() {
+
+    var container, g1, g2, g3;
+
+    function setupEqualSize() {
+      container = jasmine.svgFixture().append('g').size(200, 200);
+      g1 = container.append('g').size(10, 10).node();
+      g2 = container.append('g').size(10, 10).node();
+      g3 = container.append('g').size(10, 10).node();
+    }
+
+    function setupUnequalSize() {
+      container = jasmine.svgFixture().append('g').size(200, 200);
+      g1 = container.append('g').size(20, 10).node();
+      g2 = container.append('g').size(10, 20).node();
+      g3 = container.append('g').size(30, 30).node();
+    }
+
+    describe('horizontal', function () {
+
+      it('sets the right positions - equal', function() {
+        setupEqualSize();
+        d3.select(container.node()).layout({type: 'horizontal'});
+        expect(g1).toHaveTranslate(0,95);
+        expect(g2).toHaveTranslate(10,95);
+        expect(g3).toHaveTranslate(20,95);
+      });
+
+     it('sets the right gaps - equal', function() {
+        setupEqualSize();
+        d3.select(container.node()).layout({type: 'horizontal', gap: 10});
+        expect(g1).toHaveTranslate(0,95);
+        expect(g2).toHaveTranslate(20,95);
+        expect(g3).toHaveTranslate(40,95);
+      });
+
+      it('sets the right positions - unequal', function() {
+        setupUnequalSize();
+        d3.select(container.node()).layout({type: 'horizontal'});
+        expect(g1).toHaveTranslate(0,95);
+        expect(g2).toHaveTranslate(20,90);
+        expect(g3).toHaveTranslate(30,85);
+      });
+
+     it('sets the right gaps - unequal', function() {
+        setupUnequalSize();
+        d3.select(container.node()).layout({type: 'horizontal', gap: 10});
+        expect(g1).toHaveTranslate(0,95);
+        expect(g2).toHaveTranslate(30,90);
+        expect(g3).toHaveTranslate(50,85);
+      });
+
+    });
+
+    describe('vertical', function () {
+
+      it('sets the right positions - equal', function() {
+        setupEqualSize();
+        d3.select(container.node()).layout({type: 'vertical'});
+        expect(g1).toHaveTranslate(95, 0);
+        expect(g2).toHaveTranslate(95, 10);
+        expect(g3).toHaveTranslate(95, 20);
+      });
+
+     it('sets the right gaps - equal', function() {
+        setupEqualSize();
+        d3.select(container.node()).layout({type: 'vertical', gap: 10});
+        expect(g1).toHaveTranslate(95, 0);
+        expect(g2).toHaveTranslate(95, 20);
+        expect(g3).toHaveTranslate(95, 40);
+      });
+
+      it('sets the right positions - unequal', function() {
+        setupUnequalSize();
+        d3.select(container.node()).layout({type: 'vertical'});
+        expect(g1).toHaveTranslate(90, 0);
+        expect(g2).toHaveTranslate(95, 10);
+        expect(g3).toHaveTranslate(85, 30);
+      });
+
+     it('sets the right gaps - unequal', function() {
+        setupUnequalSize();
+        d3.select(container.node()).layout({type: 'vertical', gap: 10});
+        expect(g1).toHaveTranslate(90, 0);
+        expect(g2).toHaveTranslate(95, 20);
+        expect(g3).toHaveTranslate(85, 50);
+      });
+
+    });
+
+  });
+
+});
