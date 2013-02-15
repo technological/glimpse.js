629f15eefdc38613a9658f032d7a8f1971f64e2c
Ed Rooth <ed.rooth@rackspace.com>
2013-02-14 16:07:53 -0800

    feat(components.asset) Added simple asset component.

---
+++ src/components/asset.js
@@ -0,0 +1,119 @@
+/**
+ * @fileOverview
+ * A simple component to render out an asset.
+ */
+define([
+  'core/object',
+  'core/config',
+  'components/mixins',
+  'd3-ext/util'
+],
+function(obj, config, mixins, d3util) {
+  'use strict';
+
+  return function() {
+
+    var defaults_,
+      config_,
+      root_;
+
+    config_ = {};
+
+    defaults_ = {
+      cid: undefined,
+      assetId: undefined,
+      target: undefined,
+      cssClass: undefined
+    };
+
+    function asset() {
+      obj.extend(config_, defaults_);
+      return asset;
+    }
+
+    // Apply Mixins
+    obj.extend(
+      asset,
+      config.mixin(
+        config_,
+        'cid',
+        'assetId',
+        'target',
+        'cssClass'
+      ),
+      mixins.lifecycle,
+      mixins.toggle);
+
+    /*
+     * @public
+     * Gets the root selection of this component.
+     * @return {d3.selection}
+     */
+    asset.root = function() {
+      return root_;
+    };
+
+    /**
+     * @public
+     * Renders the component to the specified selection,
+     * or to the configured target.
+     * @param {d3.selection|Node|String}
+     * @return {components.overlay}
+     */
+    asset.render = function(selection) {
+      if (!root_) {
+        root_ = d3util.select(selection || config_.target).append('g');
+        asset.update();
+      }
+      return asset;
+    };
+
+    /**
+     * @public
+     * Triggers an update of the component reapplying all specified config
+     * updates.
+     * @return {componnets.update}
+     */
+    asset.update = function() {
+      var useEl;
+
+      if (!root_) {
+        return asset;
+      }
+      root_.attr({
+        'class': 'gl-component gl-asset'
+      });
+      if (config_.cssClass) {
+        root_.classed(config_.cssClass, true);
+      }
+      if (config_.cid) {
+        root_.attr('gl-cid', config_.cid);
+      }
+      useEl = root_.select('use');
+      if (useEl.empty()) {
+        useEl = root_.append('use');
+      }
+      useEl.attr({
+        'class': config_.assetId,
+        'xlink:href': '#' + config_.assetId
+      });
+      root_.position(config_.position);
+      return asset;
+    };
+
+    /**
+     * @public
+     * Destroys this component and cleans up after itself.
+     */
+    asset.destroy = function() {
+      root_.remove();
+      root_ = null;
+      config_ = null;
+      defaults_ = null;
+    };
+
+    return asset();
+
+  };
+
+});
