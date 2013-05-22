519f6ff41da99b83ec4dcee6bd231d6a1e23975d
Stewart Smith <stewarts@Stewarts-MacBook-Pro.local>
2013-05-22 15:14:03 -0700

    version bump 0.0.8

--- src/core/core.js
+++ src/core/core.js
@@ -13,7 +13,7 @@ function(graph, graphBuilder, component, collection, assets, pubsub) {
   'use strict';
 
   var core = {
-    version: '0.0.7',
+    version: '0.0.8',
     graphBuilder: graphBuilder,
     graph: graph,
     components: component,
